8cbba5fc9c16ced9aad687f36cf5099742f9894b
cjdepo <cjdepowell@protonmail.com>
2021-11-07 22:12:38 -0500

    added dumb computer player

--- mastermind.rb
+++ mastermind.rb
@@ -12,20 +12,51 @@ class Game
         @guess_number = 0
         @board = []
         @feedboard = []
+        @code = []
     end
 
     def self.new_game
         game = Game.new
+        game.select_options
         game.start_game
     end
 
+    def select_options
+        @player = 0
+        until @player == 1 || @player == 2
+            puts "Enter 1 to play the guesser, enter 2 to play the setter: "
+            @player = gets.chomp.to_i
+            if @player != 1 && @player != 2
+                puts "no way jose"
+            end
+        end
+    end
+
+    def start_comp_guesser
+        self.choose_secret
+    end
+
+    def start_computer_guess
+
+    end 
+
     def start_game
-        self.set_secret
+        case
+        when @player == 1
+            self.set_secret
+        when @player == 2
+            self.choose_secret
+        end
         guess_number = 0
         guess = 0
         until guess_number == self.number_guesses || guess == self.code
             guess_number += 1
-            guess = self.guess
+            case
+            when @player == 1
+                guess = self.player_guess
+            when @player == 2
+                guess = self.computer_guess
+            end
             puts "\n"
             puts "\n"
             puts "\n"
@@ -34,11 +65,17 @@ class Game
             }
             puts "\n"
         end
-        if guess == self.code
+        if guess == self.code && @player == 1
             puts "\nYou win!"
             puts "\n"
-        else 
+        elsif guess == self.code && @player == 2
+            puts "\nComputer wins!"
+            puts "\n"
+        elsif guess != self.code && @player == 1
             puts "\nYou lose!"
+            puts "\n"
+        else
+            puts "\nComputer loses!"
             puts "\n"
         end
     end
@@ -47,12 +84,21 @@ class Game
         p [@board[i], @feedboard[i]]
     end
 
+    def choose_secret
+        @number_holes.times{ |i| 
+            puts "Enter a color #{@@colors[..@number_colors - 1]}: "
+            answer = gets.chomp
+            code.push(answer)
+        }
+    end
+
+
     def set_secret
         rand_array = self.number_holes.times.map{ Random.rand(self.number_colors) }
         self.code = rand_array.map{ |i| @@colors[i] }
     end
 
-    def guess
+    def player_guess
         guess = @number_holes.times.map{ |i|
                                          answer = nil
                                          until @@colors[..self.number_colors - 1].include?(answer) 
@@ -66,6 +112,17 @@ class Game
         @feedboard.push(feedback)
         @guess_number += 1
         guess
+    end
+
+    def computer_guess
+        rand_array = self.number_holes.times.map{ Random.rand(self.number_colors) }
+        guess = rand_array.map{ |i| @@colors[i] }
+        @board[@guess_number] = guess
+        feedback = self.check(@board[@guess_number])
+        @feedboard.push(feedback)
+        @guess_number += 1
+        guess
+
     end
 
     def check(guess)
@@ -84,6 +141,5 @@ class Game
     end
 end   
 
+
 Game.new_game
-
-
