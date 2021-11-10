d8f0aa871f84ed7e959667dc7b6e0971ebd77aea
cjdepo <cjdepowell@protonmail.com>
2021-11-09 21:27:44 -0500

    functionality for computer dealing with whites, pretty buggy

--- mastermind.rb
+++ mastermind.rb
@@ -13,12 +13,13 @@ class Game
         @board = []
         @feedboard = []
         @code = []
+        @whites = []
     end
 
     def self.new_game
         game = Game.new
         game.select_options
-        game.start_game
+        game.run_game
     end
 
     def select_options
@@ -32,23 +33,10 @@ class Game
         end
     end
 
-    def start_comp_guesser
-        self.choose_secret
-    end
-
-    def start_computer_guess
-
-    end 
-
-    def start_game
-        case
-        when @player == 1
-            self.set_secret
-        when @player == 2
-            self.choose_secret
-        end
+    def run_game
+        create_secret
+        guess = nil
         guess_number = 0
-        guess = 0
         until guess_number == self.number_guesses || guess == self.code
             guess_number += 1
             case
@@ -61,7 +49,7 @@ class Game
             puts "\n"
             puts "\n"
             @board.length.times{ |i|
-                self.display(i)
+                p [@board[i], @feedboard[i]]
             }
             puts "\n"
         end
@@ -80,22 +68,18 @@ class Game
         end
     end
 
-    def display(i)
-        p [@board[i], @feedboard[i]]
-    end
-
-    def choose_secret
-        @number_holes.times{ |i| 
-            puts "Enter a color #{@@colors[..@number_colors - 1]}: "
-            answer = gets.chomp
-            code.push(answer)
-        }
-    end
-
-
-    def set_secret
-        rand_array = self.number_holes.times.map{ Random.rand(self.number_colors) }
-        self.code = rand_array.map{ |i| @@colors[i] }
+    def create_secret
+        case
+        when @player == 1
+            rand_array = number_holes.times.map{ Random.rand(self.number_colors) }
+            self.code = rand_array.map{ |i| @@colors[i] }
+        when @player == 2
+            @number_holes.times{ |i| 
+                puts "Enter a color #{@@colors[..@number_colors - 1]}: "
+                answer = gets.chomp
+                code.push(answer)
+            }
+        end
     end
 
     def player_guess
@@ -108,24 +92,55 @@ class Game
                                          answer
                                         }
         @board[@guess_number] = guess
-        feedback = self.check(@board[@guess_number])
-        @feedboard.push(feedback)
+        create_feedback(guess)
         @guess_number += 1
         guess
     end
 
     def computer_guess
         rand_array = self.number_holes.times.map{ Random.rand(self.number_colors) }
-        guess = rand_array.map{ |i| @@colors[i] }
+        init_guess = rand_array.map{ |i| @@colors[i] }
+        if @lastfeed
+            @whites = @lastfeed.map.with_index{ |v, i| v == 'white' ? @board[-1][i] : nil }
+            @white_values = @whites.compact
+            p @white_values
+            guess = init_guess.map.with_index{ |v, i| 
+                if @lastfeed[i] == 'black'
+                    @board[-1][i]
+                elsif @lastfeed[i] == 'white'
+                    if @white_values && @whites[0] != @board[-1][i]
+                        p @whites_values
+                        @white_values.pop()
+                    elsif init_guess[i] != @board[-1][i]
+                        init_guess[i]
+                    else
+                        @@colors[Random.rand(self.number_colors)]
+                    end
+                elsif @white_values.any?()
+                    p @white_values
+                    @white_values.pop()
+                else 
+                    init_guess[i]
+                end                     
+            } 
+            
+        else
+            guess = init_guess
+        end
         @board[@guess_number] = guess
-        feedback = self.check(@board[@guess_number])
-        @feedboard.push(feedback)
+        create_feedback(guess)
         @guess_number += 1
         guess
-
     end
 
-    def check(guess)
+    def create_feedback(guess)
+        feedback = self.check_guess(@board[@guess_number])
+        @feedboard.push(feedback)
+        @lastfeed = feedback
+        feedback
+    end
+
+    def check_guess(guess)
         feedback = []
         @number_holes.times{ |i|
             if guess[i].to_s == self.code[i].to_s
